96ac8cf9b30ba9f15f334a203a8668f443156e89
zmy <1370711580@qq.com>
2018-10-29 13:01:30 +0800

    some simple python

---
+++ test.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+#coding:utf-8
+#精确除法,引入库
+from __future__ import division
+
+# first
+# 输出 hello world
+#print "hello world!"
+
+#变量和赋值
+#a = 1
+#b = 0.5
+#
+#
+#a = 1
+#a = 1.5
+#a = 'aaa'
+#
+#print a
+
+#Person_name = 'zmy'
+#personName = 'zmy'
+#Personame = 'zmy'
+
+#a = 1
+#print type(a)
+#
+#b = 1.0090
+#print type(b)
+
+#字符串 '' "" ''''''
+#print "I am a student"
+#print "my name is 'chaofan'"
+#print 'my name is "charfan"'
+#print '''my name is "chaofan" 'ti' '''
+
+#str = "abcdef"
+##数组下标取值
+#print str[0]
+##切片操作 去字符串子集
+#print str[1:5] #前闭后开
+#print str[:5] #前面数字省略默认值0
+#print str[1:] #后面数字省略,默认截取到最后
+#print str[:] #字符串本身
+#print str[1:-1] #负数代表length-n
+#print str[:-20] #空串
+
+#print "my name is \r\nmy"
+
+#字符串拼接,字符串重复
+#str = "abcdef"
+#print str+'gh'
+#print str * 3
+
+#str1 = 'abcdef'
+#print len(str1) #len函数用于求字符串或其他长度的数字类型的长度
+
+#字符串格式化
+#print 'my name is %d' % 10
+
+#print True+1
+#print False - 1
+
+#print type(True)
+
+#输入函数 raw_input
+#强转 int()
+#name = raw_input()
+#print type(name)
+#print name
+#print int(name)+19
+#print type(name)
+
+#三种除法
+#传统除法
+#a = -1
+#b = 2
+#print a/b
+
+#地板除 向下取整
+#print a//b
+
+
+#乘方运算
+#c = 100 ** 100
+#print c
+
+#混合运算符
+#print 1 != 2
+#print 1 == 2
+
+#逻辑运算符
+#and or not
+
+#a = 1
+#b = 2
+#c = d = 4
+#print a != b and c == d
+#print a == b or c == d
+#print not a == b
+
+#3 < 4 and 4 < 5
+#print 3 < 4 < 5
+
+
+#()[]{}
+#列表List 元组tuple 字典dict
+#list1 = [1, 0.5, '123', True, [1,2,3]]
+#print list1[2]
+
+
+#元组修改之后无法被更改,除此之外跟list无区别
+#tuple1 = (1, 2, 3, 4)
+#print tuple1[1]
+#tuple1[1] = 3
+
+#dict 字典, 保存键值对 key-value
+dict1 = {"Ip":"127.0.0.1", "host":"www.baidu.com", "port":8080}
+print dict1["Ip"]
+print dict1["port"]
+
